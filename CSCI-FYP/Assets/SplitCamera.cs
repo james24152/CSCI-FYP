a321cc71daa52909ceb132aab2cc062a3308ebf0
KelvinLKH <31713893+1155077754@users.noreply.github.com>
2018-09-12 01:19:10 +0800

    Match up & SplitScreen
    Match up & SplitScreen

---
+++ CSCI-FYP/Assets/SplitCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitCamera : MonoBehaviour {
+    public Camera cam1;
+    public Camera cam2;
+    public Camera cam3;
+    public Camera cam4;
+    private float w;
+    private float h;
+    // Use this for initialization
+    void Start () {
+        cam1 = Camera.main;
+        cam2 = Camera.main;
+        cam3 = Camera.main;
+        cam4 = Camera.main;
+        w = 0.5f;
+        h = 0.5f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        cam1.rect = new Rect(0.0f,0.0f,0.5f,0.5f);
+        cam2.rect = new Rect(0.5f,0.0f,0.5f,0.5f);
+        cam3.rect = new Rect(0.0f,0.5f,0.5f,0.5f);
+        cam4.rect = new Rect(0.5f,0.5f,0.5f,0.5f);
+    }
+}
