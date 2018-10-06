4d3ffa7b82ac4814c22d95ac4caf38962460d7af
james24152 <james1111hk@gmail.com>
2018-10-07 02:10:17 +0800

    fire eve can now burn down trees

---
+++ CSCI-FYP/Assets/Scripts/BurnBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurnBehaviour : MonoBehaviour
+{
+
+    public GameObject fireFx;
+    public GameObject firePoint;
+    private GameObject tempFire;
+    private bool alreadyInstantiated = false;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.gameObject.CompareTag("FireAttack"))
+        {
+            if (alreadyInstantiated == false) {
+                tempFire = Instantiate(fireFx, firePoint.transform.position, firePoint.transform.rotation) as GameObject;
+                alreadyInstantiated = true;
+                Destroy(gameObject, 3f);
+                Destroy(tempFire, 3f);
+            }
+        }
+    }
+}
