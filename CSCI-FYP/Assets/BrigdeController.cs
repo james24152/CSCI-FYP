f5561ff65ac572c72ddbcdbe71e86ed4fc606b8b
Kelvin <115077754@link.cuhk.edu.hk>
2018-10-07 21:25:24 +0800

    developing bridge button

---
+++ CSCI-FYP/Assets/BrigdeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrigdeController : MonoBehaviour {
+    public GameObject button;
+    public GameObject bridge;
+    private Vector3 targetPosition;
+    // Use this for initialization
+    void Start () {
+        print("hello");
+		targetPosition = new Vector3(button.transform.position.x, button.transform.position.y-0.1f, button.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   
+}
