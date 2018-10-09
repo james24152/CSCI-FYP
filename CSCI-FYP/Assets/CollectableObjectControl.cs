a17e5604707c5e1227a615ee57c49102fac908fc
Kelvin <115077754@link.cuhk.edu.hk>
2018-10-09 15:52:37 +0800

    collectable objects + modified buttton

---
+++ CSCI-FYP/Assets/CollectableObjectControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableObjectControl : MonoBehaviour {
+    public float RotateSpeed = 20;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Time.deltaTime*RotateSpeed*2.0f, Time.deltaTime*RotateSpeed, Time.deltaTime*RotateSpeed*-1.5f);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // code of transforming element.
+
+
+        Destroy(gameObject,0.1f);
+    }
+}
