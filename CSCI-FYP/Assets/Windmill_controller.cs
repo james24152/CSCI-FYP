207f83aa70afdbb0d8415c5d7b4e9abb651ec1d1
Kelvin <115077754@link.cuhk.edu.hk>
2018-10-17 22:44:33 +0800

    fix mapAncient

--- CSCI-FYP/Assets/Windmill_controller.cs
+++ CSCI-FYP/Assets/Windmill_controller.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Windmill_controller : MonoBehaviour {
-    public GameObject sign;
     public GameObject fan;
     private WindmillFanSpin fanScript;
     private Rigidbody signRigidbody;
     private Vector3 location;
 	// Use this for initialization
 	void Start () {
-        signRigidbody = sign.GetComponent<Rigidbody>();
+        
         fanScript = fan.GetComponent<WindmillFanSpin>();
-        location = new Vector3(transform.position.x, transform.position.y - 17.3f, transform.position.z - 6.0f);
+        location = new Vector3(transform.position.x-0.3f, transform.position.y - 17.3f, transform.position.z - 6.0f);
     }
 	
 	// Update is called once per frame
@@ -22,12 +21,12 @@ public class Windmill_controller : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-        print("collide");
+        
         if(other.gameObject.tag == "WindmillSign")
         {
-            print("is sign");
+            signRigidbody = other.gameObject.GetComponent<Rigidbody>();
             fanScript.triggered = true;
-            sign.transform.position = location;
+            other.gameObject.transform.position = location;
             Destroy(signRigidbody);
         }
     }
