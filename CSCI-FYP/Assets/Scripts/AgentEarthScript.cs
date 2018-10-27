1ed3e103591574d681e8d0d38adb905b3dda161f
james24152 <james1111hk@gmail.com>
2018-10-27 23:29:18 +0800

    modified model behaviour and finished modeling enemy

--- CSCI-FYP/Assets/Scripts/AgentEarthScript.cs
+++ CSCI-FYP/Assets/Scripts/AgentEarthScript.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using MLAgents;
 using UnityEngine;
 
-public class AgentScript : Agent {
+public class AgentEarthScript : Agent
+{
 
     private Rigidbody rbody;
     private RayPerception rayPer;
@@ -15,13 +16,14 @@ public class AgentScript : Agent {
     private int wayOfDone = 0;
     private bool attackInited = false;
     private float nextTimeGetHit = 0f;
+    private Animator anim;
     //wayOfDone = 1 means collide to wall, 2 means collide to goal
 
     public GameObject player1;
     public GameObject player2;
     public GameObject player3;
     public GameObject player4;
-    public float downTime = 1f; //player down time is 2 seconds
+    public float downTime = 2f; //player down time is 2 seconds
     public float attack;
     public float speed;
     public float rayDistance = 50f;
@@ -29,6 +31,7 @@ public class AgentScript : Agent {
     public TargetScript targetScript;
     public float rotateAmount = 150f;
     public float speedScale = 8;
+    public Transform center;
 
     public Transform[] spawns;
 
@@ -37,14 +40,15 @@ public class AgentScript : Agent {
         rbody = GetComponent<Rigidbody>();
         rayPer = GetComponent<RayPerception>();
         distance = Vector3.Distance(transform.position, target.transform.position);
+        anim = GetComponent<Animator>();
         //attributes of first spawn
     }
 
     public override void CollectObservations()
     {
-        float[] rayAngles = {0f,10f,20f,30f,40f,50f,60f,70f,80f,90f,100f,110f,120f,130f,140f,150f,160f,170f,180f,190f,200f,210f,220f,230f,240f,250f,260f,270f,280f,290f,300f,310f,320f,330f,340f,350f};
-        string[] detectableObjects = { "EnemyWall", "Entrance" , "EarthEve", "WaterEve", "FireEve", "AirEve", "Agent"};
-        AddVectorObs(rayPer.Perceive(rayDistance, rayAngles, detectableObjects, 1.2f, 0f));
+        float[] rayAngles = { 0f, 10f, 20f, 30f, 40f, 50f, 60f, 70f, 80f, 90f, 100f, 110f, 120f, 130f, 140f, 150f, 160f, 170f, 180f, 190f, 200f, 210f, 220f, 230f, 240f, 250f, 260f, 270f, 280f, 290f, 300f, 310f, 320f, 330f, 340f, 350f };
+        string[] detectableObjects = { "EnemyWall", "Entrance", "EarthEve", "FireEve", "WaterEve", "AirEve", "Agent" };
+        AddVectorObs(rayPer.Perceive(rayDistance, rayAngles, detectableObjects, 0.7f, 0f));
     }
 
     public override void AgentAction(float[] vectorAction, string textAction)
@@ -58,20 +62,25 @@ public class AgentScript : Agent {
             speed = Mathf.Clamp(vectorAction[1], 0f, 1f) * speedScale;
             attack = Mathf.Clamp(vectorAction[2], -1f, 1f);
             //if attack is below 0, don't attack; if above 0, attack
-            if (attack > 0) {
-                if (!attackInited) {
+            if (attack > 0)
+            {
+                if (!attackInited)
+                {
                     InitAttack();
                     attackInited = true;
+                    anim.SetBool("Attack", true);
                 }
             }
         }
 
         //if output is discrete
-        else {
+        else
+        {
             //int action = Mathf.FloorToInt(vectorAction[0]);
             var action = Mathf.FloorToInt(vectorAction[0]);
 
-            switch (action) {
+            switch (action)
+            {
                 case 1: //turn right
                     rotateDir = transform.up * 1f;
                     break;
@@ -91,12 +100,14 @@ public class AgentScript : Agent {
         //time punishment
         AddReward(-1f / 5000f);
         //Debug.Log(Vector3.Distance(transform.position, target.transform.position));
-        if (counter == 2) {
+        if (counter == 2)
+        {
             if (distance > Vector3.Distance(transform.position, target.transform.position))
             {
                 AddReward(+0.0005f);
             }
-            else {
+            else
+            {
                 AddReward(-0.0002f);
             }
             counter = 0;
@@ -129,7 +140,8 @@ public class AgentScript : Agent {
             AddReward(-1f);
             Done();
         }
-        if (other.gameObject.layer == LayerMask.NameToLayer("Character")) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Character"))
+        {
             if (other.gameObject.CompareTag("EarthEve"))
                 AddReward(0.1f);
             if (other.gameObject.CompareTag("WaterEve"))
@@ -151,7 +163,8 @@ public class AgentScript : Agent {
 
     public override void AgentReset()
     {
-        if (wayOfDone == 1) { //collide wall
+        if (wayOfDone == 1)
+        { //collide wall
             transform.position = restartPos;
             transform.eulerAngles = restartRot;
         }
@@ -164,7 +177,8 @@ public class AgentScript : Agent {
             restartPos = transform.position;
             restartRot = transform.eulerAngles;
         }
-        if (wayOfDone == 0){
+        if (wayOfDone == 0)
+        {
             //note to self: agent will run reset once when start
             transform.position = spawns[0].position;
             restartPos = transform.position;
@@ -182,16 +196,21 @@ public class AgentScript : Agent {
         player4.GetComponent<Health>().health = 2;
     }
 
-    void InitAttack() {
+    void InitAttack()
+    {
         AddReward(-0.002f);
         Invoke("Attack", 1f);
     }
 
-    void Attack() {
+    void Attack()
+    {
         RaycastHit hit;
-        if (Physics.SphereCast(transform.position, 0.25f, transform.forward, out hit, 3f)) {
-            if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Character")) {
-                if (Time.time > nextTimeGetHit) { //this is to control the hit rate of the player
+        if (Physics.SphereCast(center.transform.position, 0.1f, transform.forward, out hit, 3f))
+        {
+            if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Character"))
+            {
+                if (Time.time > nextTimeGetHit)
+                { //this is to control the hit rate of the player
                     Debug.Log("playerhit");
                     hit.transform.gameObject.GetComponent<Health>().health--;
                     if (hit.transform.CompareTag("EarthEve"))
@@ -207,5 +226,7 @@ public class AgentScript : Agent {
             }
         }
         attackInited = false;
+        anim.SetBool("Attack", false);
     }
 }
+
