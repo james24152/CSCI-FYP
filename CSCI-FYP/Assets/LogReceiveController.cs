c26ca081e4ab0445c5dd01de1442cd04bb6d13f2
Kelvin <115077754@link.cuhk.edu.hk>
2018-11-01 18:58:09 +0800

    give logs to house

---
+++ CSCI-FYP/Assets/LogReceiveController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogReceiveController : MonoBehaviour {
+    private int woodCount = 0;
+    public ParticleSystem smokeParticle;
+	// Use this for initialization
+	void Start () {
+        smokeParticle.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void working()
+    {
+        smokeParticle.Play();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "logs")
+        {
+            woodCount = woodCount + 1;
+            Destroy(other.gameObject);
+        }
+        if (woodCount == 3)
+        {
+            working();
+        }
+    }
+}
