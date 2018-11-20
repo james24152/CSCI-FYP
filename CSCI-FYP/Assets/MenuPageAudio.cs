1a23ea5097b3cc3e1b48e961a684a34f1b412aac
Kelvin <115077754@link.cuhk.edu.hk>
2018-11-20 16:35:57 +0800

    one more push

---
+++ CSCI-FYP/Assets/MenuPageAudio.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPageAudio : MonoBehaviour {
+    private AudioManager audioManager;
+	// Use this for initialization
+	void Start () {
+		audioManager = FindObjectOfType<AudioManager>();
+        audioManager.Play("BGMBigEnough");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
