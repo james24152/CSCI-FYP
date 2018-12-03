b3759f572e9f4ab354318ae90afcc1fc81bcd346
james24152 <james1111hk@gmail.com>
2018-12-03 20:04:02 +0800

    minor changes

---
+++ CSCI-FYP/Assets/Scripts/ObjectiveTextPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveTextPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        RectTransform rect = GetComponent<RectTransform>();
+        GameObject gameManager = GameObject.FindGameObjectWithTag("GameManager");
+        Level1GameManager managerScript = gameManager.GetComponent<Level1GameManager>();
+        if (managerScript.playerCount == 1) {
+            rect.anchorMax = new Vector2(1, 1);
+            rect.anchorMin = new Vector2(1, 1);
+            rect.anchoredPosition = new Vector2(1, 1);
+            rect.pivot = new Vector2(1, 1);
+        }
+	}
+	
+}
