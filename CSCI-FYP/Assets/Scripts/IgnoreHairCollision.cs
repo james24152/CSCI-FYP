aa2c9bfa35b6f74143e86f7ee0eac06a9bbf7050
james24152 <james1111hk@gmail.com>
2019-01-28 00:26:36 +0800

    Upgraded rig joint and character material

---
+++ CSCI-FYP/Assets/Scripts/IgnoreHairCollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreHairCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Physics.IgnoreLayerCollision(10, 14);
+        Physics.IgnoreLayerCollision(8, 14);
+	}
+}
