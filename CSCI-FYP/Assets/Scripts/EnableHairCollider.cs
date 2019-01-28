dd4c44e95a592e853219b9f56a3158e78c2bb697
james24152 <james1111hk@gmail.com>
2019-01-28 11:40:44 +0800

    minor change

---
+++ CSCI-FYP/Assets/Scripts/EnableHairCollider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableHairCollider : MonoBehaviour
+{
+
+    public CapsuleCollider[] colList;
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (CapsuleCollider col in colList)
+        {
+            col.gameObject.SetActive(false);
+        }
+        Invoke("Go", 10f);
+    }
+
+    // Update is called once per frame
+    void Go()
+    {
+        foreach (CapsuleCollider col in colList)
+        {
+            col.gameObject.SetActive(true);
+        }
+    }
+
+}
