fd3192d809e135ab36bde3d0ae1d3be29a9a08dd
Kelvin <115077754@link.cuhk.edu.hk>
2019-02-11 14:36:21 +0800

    volcano v4

---
+++ CSCI-FYP/Assets/fireball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireball : MonoBehaviour {
+    Rigidbody rb;
+    public float force;
+    private float timer;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(0,0,force) , ForceMode.Force);
+        timer = Time.time + 7.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > timer)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ CSCI-FYP/Assets/fireball2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireball2 : MonoBehaviour {
+    Rigidbody rb;
+    public float force;
+    private float timer;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(0, 0, -1*force), ForceMode.Force);
+        print("add force2");
+        timer = Time.time + 7.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > timer)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
