1fcfcede2304f51c0f1cc8c61d38647f33821648
james24152 <james1111hk@gmail.com>
2019-03-02 12:10:55 +0800

    added item usage particle effect

---
+++ CSCI-FYP/Assets/Scripts/ItemFadeOutScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemFadeOutScript : MonoBehaviour {
+
+    bool on;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<CanvasRenderer>().SetAlpha(0.5f);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.RightShift) && !on)
+        {
+            Debug.Log("fire2");
+            gameObject.GetComponent<Image>().CrossFadeAlpha(1, 2.0f, false);
+            on = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightShift) && on){
+            gameObject.GetComponent<Image>().CrossFadeAlpha(0.5f, 2.0f, false);
+        }
+	}
+}
