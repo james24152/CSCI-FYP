ef1c2f54e7441a5ad94f1aabb23ba95c29407ef0
james24152 <james1111hk@gmail.com>
2019-03-05 00:31:51 +0800

    npc animation

---
+++ CSCI-FYP/Assets/Scripts/NPCBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCBehaviour : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.transform.parent.GetComponent<Animator>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.transform.name == "OuterCollider")
+        {
+            anim.SetBool("OuterCollider", true);
+        }
+        else if (gameObject.transform.name == "InnerCollider")
+        {
+            anim.SetBool("InnerCollider", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (gameObject.transform.name == "OuterCollider")
+        {
+            anim.SetBool("OuterCollider", false);
+        }
+        else if (gameObject.transform.name == "InnerCollider")
+        {
+            anim.SetBool("InnerCollider", false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
