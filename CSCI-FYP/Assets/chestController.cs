c97b5e6903c6b9bafc7fc388fd775b2a28a669dc
Kelvin <115077754@link.cuhk.edu.hk>
2019-03-05 15:29:21 +0800

    doors and chest and underground room

---
+++ CSCI-FYP/Assets/chestController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chestController : MonoBehaviour {
+    private bool triggered;
+    public bool haveKey;
+    
+	// Use this for initialization
+	void Start () {
+        triggered = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Character") && triggered == false && haveKey == true)
+        {
+            triggered = true;
+            transform.Rotate(new Vector3(-90, 0, 0));
+        }
+    }
+
+}
---
+++ CSCI-FYP/Assets/doorRight1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorRight1 : MonoBehaviour {
+    public Animator anim;
+    private bool triggered;
+    public string AnimName;
+    public bool haveKey;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        triggered = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Character") && triggered == false && haveKey == true)
+        {
+            anim.Play(AnimName);
+            triggered = true;
+        }
+        
+    }
+}
