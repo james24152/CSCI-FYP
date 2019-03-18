c880c79c6e421ff7a3daec60c4a786ad3084f713
james24152 <james1111hk@gmail.com>
2019-03-19 01:31:34 +0800

    modified boss behaviour

--- CSCI-FYP/Assets/Scripts/BossBehaviour.cs
+++ CSCI-FYP/Assets/Scripts/BossBehaviour.cs
@@ -12,17 +12,69 @@ public class BossBehaviour : MonoBehaviour {
     public ParticleSystem swordAura;
     public ParticleSystem proj;
     public Transform origin;
-    private ParticleSystem tempAttack;
+    public int health = 100;
 
+    private ParticleSystem tempAttack;
+    private SphereCollider collider;
+    private Animator anim;
+    private bool far;
+    private bool healthLow;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        collider = GetComponent<SphereCollider>();
+        anim = GetComponent<Animator>();
+        anim.SetBool("FarAway", true);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Character")) {
+            anim.SetBool("FarAway", false); //boss will initiate melee attack
+        }
+    }
+
+    private void Tactics() {
+        far = anim.GetBool("FarAway");
+        healthLow = health < 80 ? true : false;
+        if (far)
+        {
+            if (healthLow)
+            {
+                InitiatePushAndHeal();
+            }
+            else {
+                InitiateProjectile();
+                //randomly init projectile and aoe
+            }
+        }
+        else {
+            InitiateSwing();
+        }
+    }
+
+    public void InitiateProjectile() {
+        anim.SetBool("UseProj", true);
+    }
+
+    public void InitiateSwing()
+    {
+        anim.SetBool("Swing", true);
+    }
+
+    public void InitiateAOE()
+    {
+        anim.SetBool("UseAOE", true);
+    }
+
+    public void InitiatePushAndHeal()
+    {
+        anim.SetBool("NeedHeal", true);
+    }
 
     public void FireProj()
     {
