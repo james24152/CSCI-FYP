8dccf5b8a9583f8915ca54b058afb0dc3bf0bea4
Kelvin <115077754@link.cuhk.edu.hk>
2019-03-19 14:53:29 +0800

    hints in medieval and wind zone

---
+++ CSCI-FYP/Assets/Wind.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour {
+    public float force;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerStay(Collider other)
+    {
+        
+        if (other.gameObject.layer == LayerMask.NameToLayer("Character"))
+        {
+            print("inside Wind Zone");
+            //print(other.GetComponent<Rigidbody>());
+            other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(-1.0f*force,0.0f,0.0f));
+            
+        }
+    }
+}
